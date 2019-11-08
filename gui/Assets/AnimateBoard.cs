ae6dcba4cd3418df650e1031f8fbba18743aac79
Nancy Alarabawy <nanciie.arx@gmail.com>
2019-11-08 14:32:27 +0200

    GUI Added

---
+++ gui/Assets/AnimateBoard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateBoard : MonoBehaviour
+{
+    public float x_speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, x_speed, 0);
+        
+    }
+}
---
+++ gui/Assets/KeyBinding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBinding : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys.Add("Up", KeyCode.UpArrow);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keys["Up"]))
+        {
+            Debug.Log("Up Pressed");
+        }
+        
+    }
+}
