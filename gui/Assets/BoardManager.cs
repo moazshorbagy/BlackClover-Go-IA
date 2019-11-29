d6ebbf10b0610c8c9a081d5fff354657b39e08c5
ahmadhammam1 <50032163+ahmadhammam1@users.noreply.github.com>
2019-11-29 20:07:13 +0200

    ok

--- gui/Assets/BoardManager.cs
+++ gui/Assets/BoardManager.cs
@@ -2,18 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace Board
+{
+    
+
 public class BoardManager : MonoBehaviour
 {
     
     
     private const int NumberOfTiles=19;
     private int[] Selction={-1,-1};
-    public List <GameObject> Stones;
+    public GameObject particle;
+    public List <GameObject> Stones; // a list containg a white stone and a black stone
+    Dictionary<Vector2, GameObject> SpawnedStones = new Dictionary<Vector2, GameObject>();
 
-    void SpawnStones(int index,Vector3 position)
+/*this Function takes an index which indicates which stone to spawn, index is 0 for black stones and 1 for white stones
+  it's second parameter is the position to spawn the stone at position should range from 0.0f to 18.0f in x and y directions.
+*/
+    void SpawnStones(int index,Vector2 position)
     {
-        GameObject go = Instantiate(Stones[index],position,Quaternion.identity)as GameObject;
-        go.transform.SetParent(transform);
+        float XPos=1.2f+(position.x*1.2f);
+        float YPos=1.2f+(position.y*1.2f);
+        Vector3 MappedPosition =new Vector3(XPos,0.3f,YPos);
+        GameObject Stone = Instantiate(Stones[index],MappedPosition,Quaternion.identity)as GameObject;
+        Stone.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+        Stone.transform.SetParent(transform);
+        SpawnedStones.Add(position,Stone);
+
+    }
+/*
+This Function takes and array of positions to delete the stones present at these locations 
+It returns true if all stones where deleted successfully
+It returns false if there is no stone at any position in the passed array , Throwing a KeyNotFoundException
+*/
+    bool RemoveStones(Vector2[] arrayOfPositions)
+    {
+        bool allDeleted =true;
+        foreach(Vector2 position in arrayOfPositions) 
+        {
+        try
+        {
+            Destroy(SpawnedStones[position]);     
+        }
+        catch (KeyNotFoundException)
+        {
+            allDeleted=false;  
+        }
+        }
+         return allDeleted;
 
     }
 
@@ -22,40 +58,23 @@ public class BoardManager : MonoBehaviour
     {
         
          
-            Vector3 position= new Vector3(1.2f,0.3f,0.6f);
+            Vector2 position;
+            
+            position= new Vector2(2.0f,0.0f);
             SpawnStones(0,position);
+
+             
+            
+            position=new Vector2(1.0f,0.0f);
+           
+            GameObject Stone =SpawnedStones[position];
+            Destroy(Stone);
         
 
 
     }
-    void DrawGoBoard()
+    //Test function will be implemented later
 
-    {
-        Vector3 width = Vector3.right *NumberOfTiles;
-        Vector3 height = Vector3.forward *NumberOfTiles;
-        for (int i=0;i<=NumberOfTiles;i++)
-        {
-            Vector3 start=Vector3.forward *i;
-            Debug.DrawLine(start,start+width);
-            for (int j=0;j<=NumberOfTiles;j++)
-        {
-            start=Vector3.right *j;
-            Debug.DrawLine(start,start+height);
-        }
-
-        }
-     if(Selction[0]>=0 && Selction[1]>=0)
-        {
-            Debug.DrawLine(
-                Vector3.forward*Selction[1] +Vector3.right*Selction[0],
-                Vector3.forward*(Selction[1]+1)+Vector3.right*(Selction[0]+1));
-                Debug.DrawLine(
-                Vector3.forward*(Selction[1]+1) +Vector3.right*(Selction[0]),
-                Vector3.forward*(Selction[1])+Vector3.right*(Selction[0]+1));
-        }
-
-
-    }
     void UpdateSelection()
     {
         if(!Camera.main)
@@ -68,19 +87,24 @@ public class BoardManager : MonoBehaviour
             
             Selction[0]=(int) hit.point.x;
             Selction[1]=(int) hit.point.z;
+            Debug.Log("X= " + hit.point.x);
+            Debug.Log("Z= " + hit.point.z);
+
         }
         else{
             Selction[0]=-1;
             Selction[1]=-1;
 
         }
+         
 
     }
+    
     void Update()
     {
         UpdateSelection();
+
        
-        DrawGoBoard();
-        
     }
+}
 }
---
+++ gui/Assets/StoneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Board
+{
+    public class StoneManager
+    {
+        GameObject whiteStone;
+        GameObject blackStone;
+
+        public StoneManager()
+        {
+            
+           // var loadedObj = new OBJLoader().Load(@"GO_Assets\Black Stone.fbx");
+
+        }
+        
+    }
+    
+}
